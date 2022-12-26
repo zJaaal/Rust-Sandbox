e4842fdd9020524ec206157a16ffe90f178704de
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-26 14:08:26 -0300

    mutable variables

--- rust-sandbox/src/main.rs
+++ rust-sandbox/src/main.rs
@@ -4,5 +4,20 @@ fn main() {
     println!(
         "Hi I'm {} and I'm learning {} on {}/{}/{}",
         "Jal", "Rust", 26, 12, 2022
-    )
+    );
+
+    //This is inmutable variables
+    let name = "Jalinson";
+    //Thi is mutable variable
+    let mut age = 21;
+
+    println!("My name is {} and I'm {} years old", name, age);
+
+    //That means this is wrong:
+    // name = "Jose"
+
+    //But we can mutate this onw
+    age += 1;
+
+    println!("Now I'm {} years old", age);
 }
