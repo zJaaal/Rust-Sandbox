667c6bdead5f015bb3eb55c268f54322e4f8a5bd
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-26 14:09:31 -0300

    variable shadowing

--- rust-sandbox/src/main.rs
+++ rust-sandbox/src/main.rs
@@ -20,4 +20,15 @@ fn main() {
     age += 1;
 
     println!("Now I'm {} years old", age);
+
+    // Declare first variable binding with name "shadow_num"
+    let shadow_num = 5;
+
+    // Declare second variable binding, shadows existing variable "shadow_num"
+    let shadow_num = shadow_num + 5;
+
+    // Declare third variable binding, shadows second binding of variable "shadow_num"
+    let shadow_num = shadow_num * 2;
+
+    println!("The number is {}.", shadow_num);
 }
