a88a0ae35c761275a9cf211d41b1242d905f63da
Jalinson Diaz <zjaaaldev@gmail.com>
2022-12-26 14:03:14 -0300

    First Rust steps

---
+++ rust-sandbox/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    //This way you can print variables in a template
+    println!(
+        "Hi I'm {} and I'm learning {} on {}/{}/{}",
+        "Jal", "Rust", 26, 12, 2022
+    )
+}
